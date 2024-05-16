c2d2e195b0a04350e5a105553de3f799a5ffb985
Renad Abdelhay <abdelhar@gmatc.matc.edu>
2024-05-15 23:07:21 -0500

    Final Ptoject

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from "./pages/Home"
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" exact component={Home}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
